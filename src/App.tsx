689f9919050c6f14c916b75956cf328faa39b772
Erwan Rossignol <erwan@hotmail.ch>
2025-10-23 15:54:50 +0200

    Initial commit: setup React + TypeScript + Tailwind

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen bg-gray-900 text-white">
+      <h1 className="text-4xl font-bold text-blue-400 mb-4">
+        Hello Tailwind + React + TypeScript 🎉
+      </h1>
+      <p className="text-gray-300">Si tu vois ça, tout fonctionne ✅</p>
+    </div>
+  )
+}
+
+export default App
